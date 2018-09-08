4f41a726aac814655a88a31a90b1cdf2b0cc780c
DESKTOP-BRIGC4V\USER <shuna966@gmail.com>
2018-09-08 13:37:56 +0900

    180908

--- stableSort.py
+++ stableSort.py
@@ -1,6 +1,15 @@
 N=int(input())
 
 c = list(map(str, input().split()))
+# d = list(map(str, input().split()))
+import copy
+# ポインタではなく、値を渡す
+d = copy.deepcopy(c)
+default = copy.deepcopy(c)
+
+
+
+
 
 # card 
 # get partial str by "slice"
@@ -18,7 +27,7 @@ def bubble(C_,N_):
 def selection(C_,N_):
     for i in range(N_):
         minj = i
-        for j in (i,N_):
+        for j in range(i,N_):
             if C_[j][1:2] < C_[minj][1:2]:
                 minj = j
         t=C_[i]
@@ -27,11 +36,95 @@ def selection(C_,N_):
     
     return C_
 
-# cb = ' '.join(bubble(c,N))
+def getCommon(C_,N_):
+    common=[]
+    for i in range(N):
+        for j in range(i,N):
+            if C_[i][1:2]==C_[j][1:2] and i!=j:
+                common.append([C_[i],C_[j]])
 
-cs = ' '.join(selection(c,N))
+    for val in common:
+        ret = val
 
-print(cs)
+    return ret
+
+
+b = bubble(c,N)
+a = selection(d,N)
+gc = getCommon(default,N)
+
+
+
+
+
+
+print(a)
+print(b)
+print(gc)
+
+
+
+# print(getCommon(b,N))
+
+# print(getCommon(a,N))
+
+# print(getCommon(b,N))
+
+
+
+
+
+
+
+
+
+
+# cb = ' '.join(bubble(c,N)
+
+# cs = ' '.join(selection(c,N))
+
+# a = selection(c,N)
+# b=[]
+# for i in range(len(a)):
+    # print(a[i][1:2])
+    # if a[i] not in b[i][1:2]:
+        # b.append(a[i])
+    #     a.pop(a[i])
+    #     print(a)
+
+
+
+
+
+
+
+
+
+
+
+# print(a)
+# print(b)
+
+
+# print(a[1])
+# print(a[2])
+
+
+
+
+# print(selection(c,N))
+
+
+# b=[]
+# for i in a[1:2]:
+#     print(i)
+    # if i not in b:
+    #     b.append(i)
+
+
+# print(i)
+
+# print(cs)
 
 # print(c)
 
