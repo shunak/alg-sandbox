be76e89c24ff25b900e0e04fc7f608e03309e61d
shunak <shuna966@gmail.com>
2020-07-13 10:51:44 +0900

    all_count

--- duplication_check_all_count.cpp
+++ duplication_check_all_count.cpp
@@ -14,19 +14,35 @@
 #define si(n) scanf("%lld",&n)
 #define si3(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
 #define pt(n) printf("%lld\n",n)
+#define pt_nnl(n) printf("%lld ",n) //nnl =no newline
 #define ll long long
+#define nl puts("")
 using namespace std;
 
 int main()
 {
-    ll k,n,A[200000];
+    ll k,n,A[200000],B[200000],Same=-1,No=-1;
 
     si(n);
-    lp(i,n)
+    lp(i,n){
         si(A[i]);
+    }
+    lp(i,n)B[A[i]]++;
 
-    lp(i,n)
-        pt(A[i]);
+    for (ll i = 1; i <= n; i++) {
+        if(B[i]==0)No=i;
+        if(B[i]==2)Same=i;
+    }
+    if(Same==-1)puts("Correct");
+    else printf("%lld %lld\n",Same,No);
+
+
+
+    /* lp(i,n) */
+    /*     pt_nnl(A[i]); */
+    /* lp(i,n) */
+    /*     pt_nnl(B[i]); */
+    /* nl; */
 
     return 0;
 }
