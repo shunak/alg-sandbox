e847f1a02fa44de232cb2238b9a618232164b55f
shunak <shuna966@gmail.com>
2020-07-31 12:41:09 +0900

    add comment

--- greedy_method.cpp
+++ greedy_method.cpp
@@ -7,6 +7,7 @@ int main(){
     cin >> N;
     cin >> K;
     for(int i = 0; i < N; ++i) {
+        // now*2 or now+K, choose less than value, and update now val
         now = min(now*2,now+K);
     }
     cout << now<< endl;
